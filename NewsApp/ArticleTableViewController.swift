8eb84c593da1a5a4cea2ad6a56d8a2fa4730d3c2
Eric Cook <office@bettersearchllc.com>
2020-04-27 12:25:44 -0400

    4-27-20

--- NewsApp/ArticleTableViewController.swift
+++ NewsApp/ArticleTableViewController.swift
@@ -7,62 +7,100 @@
 
 import UIKit
 import Kingfisher
+import SafariServices
 
-class ArticleTableViewController: UITableViewController {
+class ArticleTableViewController: UITableViewController, SFSafariViewControllerDelegate {
 
     var articles = [Article]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         getArticles()
         
         tableView.separatorStyle = .none
         
         _ = Timer.scheduledTimer(timeInterval: 180, target: self, selector: #selector(getArticles), userInfo: nil, repeats: true)
+        
+        
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        
+        return .lightContent
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        navigationController?.navigationBar.barStyle = .black
+        
     }
     
     @objc func getArticles() {
+        
         NewsHelper().getArticles { (articles) in
              DispatchQueue.main.async {
                 self.articles = articles
                 self.tableView.reloadData()
             }
         }
-        //print("Articles refreshed! \(articles.first?.source)")
+     
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return articles.count
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         if let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell", for: indexPath) as? ArticleCell {
 
             let article = articles[indexPath.row]
             
             cell.titleLabel.text = article.title
             cell.categoryLabel.text = article.category.rawValue
-            //cell.sourceLabel.text = article.source
             cell.categoryLabel.backgroundColor = article.categoryColor
             
             let url = URL(string: article.urlToImage)
+            
             cell.articleImageView.kf.setImage(with: url)
-            //cell.articleImageView.kf.setImage(with: url, placeholder: UIImage(named:"Filler"), options: nil, progressBlock: nil, completionHandler: nil)
-
+            
             return cell
         }
+        
         return UITableViewCell()
+        
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
         return 260
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
         let article = articles[indexPath.row]
-        performSegue(withIdentifier: "goToURL", sender: article)
+        
+        if let url = URL(string: article.url) {
+            
+            let safariVC = SFSafariViewController(url: url)
+            
+            safariVC.preferredBarTintColor = .darkGray
+            
+            safariVC.preferredControlTintColor = .white
+            
+            self.present(safariVC, animated: true, completion: nil)
+            
+            safariVC.delegate = self
+            
+        }
+        
+    }
+    
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        controller.dismiss(animated: true, completion: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -76,7 +114,9 @@ class ArticleTableViewController: UITableViewController {
     }
     
     @IBAction func reloadTapped(_ sender: Any) {
+        
         getArticles()
+        
     }
     
 }
@@ -85,5 +125,5 @@ class ArticleCell : UITableViewCell {
     @IBOutlet weak var articleImageView: UIImageView!
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
-    //@IBOutlet weak var sourceLabel: UILabel!
+   
 }
--- NewsApp/ArticleWebViewController.swift
+++ NewsApp/ArticleWebViewController.swift
@@ -8,21 +8,206 @@
 
 import UIKit
 import WebKit
+import MessageUI
+import SafariServices
 
-class ArticleWebViewController: UIViewController {
+class ArticleWebViewController: UIViewController, SFSafariViewControllerDelegate, MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate {
     
     var article = Article()
+    
+    var emailSent = 0
+       
+    var textSent = 0
+    
+     @IBAction func textButton(_ sender: Any) {
+        
+        sendText()
+        
+    }
+    
+    @IBAction func emailtButton(_ sender: Any) {
+           
+        sendEmail()
+        
+       }
 
     @IBOutlet weak var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if let url = URL(string: article.url) {
         
+        webView.configuration.mediaTypesRequiringUserActionForPlayback = .video
+        
+        if let url = URL(string: article.url) {
+            
             webView.load(URLRequest(url: url))
+            
+            let safariVC = SFSafariViewController(url: url)
+            
+            present(safariVC, animated: true, completion: nil)
+            
+            /*let safariVC = SFSafariViewController(url: url)
+            self.present(safariVC, animated: true, completion: nil)
+            safariVC.delegate = self*/
+           
         }
+        
+        let addText = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(sendText))
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        
+        let addEmail = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(sendEmail))
+       
+        toolbarItems = [addText, spacer, addEmail]
+        
+        navigationController?.setToolbarHidden(false, animated: false)
     }
+    
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func sendText() {
+        
+        let messageVC = MFMessageComposeViewController()
+        
+        messageVC.subject = "Check out this article!"
+        
+        messageVC.body =  "Link: " + self.article.url
+        
+        messageVC.messageComposeDelegate = self;
+        
+        self.present(messageVC, animated: false, completion: nil)
+        
+    }
+    
+    @objc func sendEmail() {
+        
+        let mc = MFMailComposeViewController()
+        
+        mc.mailComposeDelegate = self
+        
+        mc.setSubject("Check out this article!")
+        
+        mc.setMessageBody("Link: " + self.article.url, isHTML: true)
+        
+        self.present(mc, animated: true, completion: nil)
+        
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        switch (result.rawValue) {
+        case MessageComposeResult.cancelled.rawValue:
+            print("Message was cancelled")
+            
+            self.dismiss(animated: true, completion: nil)
+        case MessageComposeResult.failed.rawValue:
+            print("Message failed")
+            
+            self.dismiss(animated: true, completion: nil)
+        case MessageComposeResult.sent.rawValue:
+            print("Message was sent")
+            textSent = 1
+            self.dismiss(animated: true, completion: nil)
+        default:
+            break;
+        }
+        
+        textSentSuccess()
+    }
+    
+    
+    func mailComposeController(_ controller:MFMailComposeViewController, didFinishWith result:MFMailComposeResult, error:Error?) {
+        
+        switch result.rawValue {
+        case MFMailComposeResult.cancelled.rawValue:
+            print("Mail cancelled")
+            
+        case MFMailComposeResult.saved.rawValue:
+            print("Mail saved")
+            
+        case MFMailComposeResult.sent.rawValue:
+            print("Mail sent")
+            emailSent = 1
+        case MFMailComposeResult.failed.rawValue:
+            print("Mail sent failure: \(error!.localizedDescription)")
+            
+        default:
+            break
+        }
+        self.dismiss(animated: true, completion: nil)
+        emailSentSuccess()
+    }
+    
+    func emailSentSuccess() {
+        
+        if emailSent == 0 {
+            
+            let alert = UIAlertController(title: "Your email failed to send or was cancelled.", message: "", preferredStyle: UIAlertController.Style.alert)
+            
+            
+            alert.addAction(UIAlertAction(title: "Done", style: .cancel, handler: { action in
+                
+                alert.dismiss(animated: true, completion: nil)
+                
+            }))
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        } else {
+            
+            let alert = UIAlertController(title: "Your email has been sent.", message: "", preferredStyle: UIAlertController.Style.alert)
+            
+            
+            alert.addAction(UIAlertAction(title: "Done", style: .cancel, handler: { action in
+                
+                alert.dismiss(animated: true, completion: nil)
+                
+            }))
+            
+            self.present(alert, animated: true, completion: nil)
+            
+            emailSent = 0
+        }
+        
+        
+    }
+    
+    func textSentSuccess() {
+        
+        if textSent == 0 {
+            
+            let alert = UIAlertController(title: "Your text failed to send or was cancelled.", message: "", preferredStyle: UIAlertController.Style.alert)
+            
+            
+            alert.addAction(UIAlertAction(title: "Done", style: .cancel, handler: { action in
+                
+                alert.dismiss(animated: true, completion: nil)
+                
+            }))
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        } else {
+            
+            let alert = UIAlertController(title: "Your text has been sent.", message: "", preferredStyle: UIAlertController.Style.alert)
+            
+            
+            alert.addAction(UIAlertAction(title: "Done", style: .cancel, handler: { action in
+                
+                alert.dismiss(animated: true, completion: nil)
+                
+            }))
+            
+            self.present(alert, animated: true, completion: nil)
+            
+            textSent = 0
+            
+        }
+        
+        
+    }
+    
 
     
 }
